e17612202223522a84ec6a1a3230d946e7fd2531
Regis18 <regis.humana@fundacion-jala.org>
2019-06-17 15:26:57 -0400

    Erase

--- vagrant2/install_jenkins.sh
+++
@@ -1,7 +0,0 @@
-apt-get update
-apt-get install openjdk-11-jdk -y
-
-wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
-sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-apt-get update
-apt-get install jenkins -y
